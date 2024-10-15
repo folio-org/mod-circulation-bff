cd037f9042fb22780c373f05bc87ef5c5622d7c5
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-10-15 15:26:27 +0300

    MCBFF-4 Remove redundant code

--- src/test/java/org/folio/circulationbff/service/SearchServiceTest.java
+++ src/test/java/org/folio/circulationbff/service/SearchServiceTest.java
@@ -8,18 +8,12 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-import org.folio.circulationbff.client.feign.HoldingsStorageClient;
-import org.folio.circulationbff.client.feign.ItemStorageClient;
-import org.folio.circulationbff.client.feign.LocationClient;
-import org.folio.circulationbff.client.feign.MaterialTypeClient;
 import org.folio.circulationbff.client.feign.SearchClient;
-import org.folio.circulationbff.client.feign.ServicePointClient;
 import org.folio.circulationbff.domain.dto.BffSearchInstance;
 import org.folio.circulationbff.domain.dto.SearchInstance;
 import org.folio.circulationbff.domain.dto.SearchInstances;
 import org.folio.circulationbff.domain.mapping.SearchInstanceMapper;
 import org.folio.circulationbff.service.impl.SearchServiceImpl;
-import org.folio.spring.service.SystemUserScopedExecutionService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,14 +22,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class SearchServiceTest {
-  @Mock private ItemStorageClient itemStorageClient;
-  @Mock private HoldingsStorageClient holdingsStorageClient;
-  @Mock private LocationClient locationClient;
-  @Mock private MaterialTypeClient materialTypeClient;
-  @Mock private ServicePointClient servicePointClient;
   @Mock private SearchClient searchClient;
-  @Mock private SystemUserScopedExecutionService executionService;
-  @Mock private BulkFetchingService fetchingService;
   @Mock private SearchInstanceMapper searchInstanceMapper;
 
   @InjectMocks
@@ -57,16 +44,6 @@ class SearchServiceTest {
     BffSearchInstance bffSearchInstance = new BffSearchInstance().id(instanceId);
     when(searchInstanceMapper.toBffSearchInstance(searchInstance))
       .thenReturn(bffSearchInstance);
-//
-//    when(itemStorageClient.getByQuery(any(CqlQuery.class)))
-//      .thenReturn(new Items());
-//
-//    when(holdingsStorageClient.getByQuery(any(CqlQuery.class)))
-//      .thenReturn(new HoldingsRecords());
-//
-//    when(holdingsStorageClient.getByQuery(any(CqlQuery.class)))
-//      .thenReturn(new HoldingsRecords());
-
 
     Collection<BffSearchInstance> response = searchService.findInstances(query);
     assertThat(response, equalTo(List.of(bffSearchInstance)));
