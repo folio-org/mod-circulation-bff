9e33dde57d057a1ee31afc2ddd99a94d8ce2d764
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-10-18 15:56:32 +0300

    MCBFF-4 Tests for CqlQuery

---
+++ src/test/java/org/folio/circulationbff/support/CqlQueryTest.java
@@ -0,0 +1,42 @@
+package org.folio.circulationbff.support;
+
+import static java.util.Collections.emptyList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class CqlQueryTest {
+
+  @Test
+  void exactMatchAnyThrowsExceptionWhenCollectionOfValuesIsEmpty() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+      () -> CqlQuery.exactMatchAny("index", emptyList()));
+    assertThat(exception.getMessage(), is("Values cannot be null or empty"));
+  }
+
+  @Test
+  void exactMatchAnyThrowsExceptionWhenCollectionOfValuesIsNull() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+      () -> CqlQuery.exactMatchAny("index", null));
+    assertThat(exception.getMessage(), is("Values cannot be null or empty"));
+  }
+
+  @Test
+  void exactMatchAnyThrowsExceptionWhenIndexIsNull() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+      () -> CqlQuery.exactMatchAny(null, List.of("value")));
+    assertThat(exception.getMessage(), is("Index cannot be blank"));
+  }
+
+  @Test
+  void exactMatchAnyThrowsExceptionWhenIndexIsEmptyString() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+      () -> CqlQuery.exactMatchAny("", List.of("value")));
+    assertThat(exception.getMessage(), is("Index cannot be blank"));
+  }
+
+}
