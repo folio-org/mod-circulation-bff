1288d5451bd1945d3fc0f7a1f40a4070c53b0f72
MagzhanArtykov <magzhan_artykov@epam.com>
2024-10-26 03:25:12 +0500

    MCBFF-6 BFF for create request endpoints

--- src/main/java/org/folio/circulationbff/support/JacksonConfig.java
+++ src/main/java/org/folio/circulationbff/support/JacksonConfig.java
@@ -1,5 +1,8 @@
 package org.folio.circulationbff.support;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 import org.folio.circulationbff.domain.dto.BffRequest;
 import org.folio.circulationbff.domain.dto.RequestRequester;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +18,9 @@ public class JacksonConfig {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.addMixIn(RequestRequester.class, RequestRequesterMixin.class);
     objectMapper.addMixIn(BffRequest.class, BffRequestMixin.class);
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+    dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+    objectMapper.setDateFormat(dateFormat);
     return objectMapper;
   }
 }
