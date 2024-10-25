67a287064d828b88b374286687e0972c520524c1
MagzhanArtykov <magzhan_artykov@epam.com>
2024-10-25 17:43:14 +0500

    MCBFF-6 BFF for create request endpoints

---
+++ src/main/java/org/folio/circulationbff/support/BffRequestMixin.java
@@ -0,0 +1,7 @@
+package org.folio.circulationbff.support;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public interface BffRequestMixin {
+}
--- src/main/java/org/folio/circulationbff/support/JacksonConfig.java
+++ src/main/java/org/folio/circulationbff/support/JacksonConfig.java
@@ -1,5 +1,6 @@
 package org.folio.circulationbff.support;
 
+import org.folio.circulationbff.domain.dto.BffRequest;
 import org.folio.circulationbff.domain.dto.RequestRequesterPatronGroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ public class JacksonConfig {
   public ObjectMapper objectMapper() {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.addMixIn(RequestRequesterPatronGroup.class, RequestRequesterPatronGroupMixin.class);
+    objectMapper.addMixIn(BffRequest.class, BffRequestMixin.class);
     return objectMapper;
   }
 }
