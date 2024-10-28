e464edb928ebaf382585f86ca0fb455740b7edd7
MagzhanArtykov <magzhan_artykov@epam.com>
2024-10-28 19:06:42 +0500

    MCBFF-6 Added unit tests

--- src/main/java/org/folio/circulationbff/controller/CirculationBffController.java
+++ src/main/java/org/folio/circulationbff/controller/CirculationBffController.java
@@ -8,8 +8,6 @@ import java.util.UUID;
 
 import org.folio.circulationbff.domain.dto.AllowedServicePointParams;
 import org.folio.circulationbff.domain.dto.AllowedServicePoints;
-import org.folio.circulationbff.domain.dto.BffSearchInstance;
-import org.folio.circulationbff.domain.dto.EmptyBffSearchInstance;
 import org.folio.circulationbff.domain.dto.BffRequest;
 import org.folio.circulationbff.domain.dto.BffSearchInstance;
 import org.folio.circulationbff.domain.dto.EmptyBffSearchInstance;
