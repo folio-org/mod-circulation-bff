2abe5ef7e28efaaebeb30f3d3f9f561e510f01d6
MagzhanArtykov <magzhan_artykov@epam.com>
2024-10-28 15:36:17 +0500

    MCBFF-6 Added unit tests

--- src/main/java/org/folio/circulationbff/controller/CirculationBffController.java
+++ src/main/java/org/folio/circulationbff/controller/CirculationBffController.java
@@ -127,6 +127,7 @@ public class CirculationBffController implements CirculationBffApi {
 
   @Override
   public ResponseEntity<BffRequest> createRequest(String tenantId, BffRequest bffRequest) {
+    log.info("createRequest:: tenantId: {}, bffRequest: {}", tenantId, bffRequest.getId());
     return ResponseEntity.status(CREATED)
       .body(circulationBffService.createRequest(bffRequest, tenantId));
   }
