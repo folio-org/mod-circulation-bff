8a9bf2a6b23dbecfd8b3ce46d9670f62b8f96f27
MagzhanArtykov <magzhan_artykov@epam.com>
2024-11-07 16:31:00 +0500

    MCBFF-6 BFF for "create request" endpoints

--- src/main/java/org/folio/circulationbff/support/BffRequestMixin.java
+++
@@ -1,7 +0,0 @@
-package org.folio.circulationbff.support;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public interface BffRequestMixin {
-}
--- src/main/java/org/folio/circulationbff/support/RequestRequesterMixin.java
+++
@@ -1,8 +0,0 @@
-package org.folio.circulationbff.support;
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-public abstract class RequestRequesterMixin {
-  @JsonDeserialize(using = PatronGroupDeserializer.class)
-  private Object patronGroup;
-}
