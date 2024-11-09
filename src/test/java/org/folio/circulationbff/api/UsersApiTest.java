e924824a8f37e7d6c7c23886676fe4d0d6b5aaa6
alexanderkurash <alexander.kurash@gmail.com>
2024-11-09 18:01:19 +0200

    MCBFF-7 Refactor tests

--- src/test/java/org/folio/circulationbff/api/UsersApiTest.java
+++ src/test/java/org/folio/circulationbff/api/UsersApiTest.java
@@ -4,6 +4,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.jsonResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+import static java.lang.String.format;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,38 +26,31 @@ class UsersApiTest extends BaseIT {
 
   private static final String EXTERNAL_USER_ID_PARAM = "externalUserId";
   private static final String TENANT_ID_PARAM = "tenantId";
-  private static final String CIRCULATION_BFF_URL =
-    String.format("/circulation-bff/external-users/{%s}/tenant/{%s}",
-      EXTERNAL_USER_ID_PARAM, TENANT_ID_PARAM);
-  private static final String MOD_USERS_PARAM = "externalSystemId==externalUserId";
+  private static final String CIRCULATION_BFF_URL = format(
+    "/circulation-bff/external-users/{%s}/tenant/{%s}", EXTERNAL_USER_ID_PARAM, TENANT_ID_PARAM);
+  private static final String FILTER_BY_EXTERNAL_SYSTEM_ID_QUERY =
+    "externalSystemId==externalUserId";
   private static final String MOD_USERS_URL = "/users";
   private static final String PARAM_QUERY = "query";
 
   @Test
   @SneakyThrows
   void getUsersByQueryTest() {
-    UserCollection expected = buildUsers();
+    UserCollection users = new UserCollection(List.of(new User()), 1);
 
     wireMockServer.stubFor(WireMock.get(urlPathMatching(MOD_USERS_URL))
-      .withQueryParam(PARAM_QUERY, equalTo(MOD_USERS_PARAM))
-      .willReturn(jsonResponse(expected, HttpStatus.SC_OK)));
+      .withQueryParam(PARAM_QUERY, equalTo(FILTER_BY_EXTERNAL_SYSTEM_ID_QUERY))
+      .willReturn(jsonResponse(users, HttpStatus.SC_OK)));
 
     mockMvc.perform(
         get(CIRCULATION_BFF_URL, EXTERNAL_USER_ID_PARAM, TENANT_ID_PARAM)
           .headers(defaultHeaders())
           .contentType(MediaType.APPLICATION_JSON))
       .andExpect(status().isOk())
-      .andExpect(content().json(Json.write(expected)));
+      .andExpect(content().json(Json.write(users)));
 
     wireMockServer.verify(1, getRequestedFor(urlPathMatching(MOD_USERS_URL))
-      .withQueryParam(PARAM_QUERY, equalTo(MOD_USERS_PARAM)));
-  }
-
-  private static UserCollection buildUsers() {
-    UserCollection mockedUsers = new UserCollection();
-    User mockedUser = new User();
-    mockedUsers.setUsers(List.of(mockedUser));
-    return mockedUsers;
+      .withQueryParam(PARAM_QUERY, equalTo(FILTER_BY_EXTERNAL_SYSTEM_ID_QUERY)));
   }
 
 }
