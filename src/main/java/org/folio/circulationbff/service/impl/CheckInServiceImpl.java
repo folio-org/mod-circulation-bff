590a45a6c1511ae4a3a90c508609c3189ddf2286
imerabishvili <144257054+imerabishvili@users.noreply.github.com>
2025-01-27 19:38:10 +0200

    MCBFF-38: Show real effective location during checkin (#47)
    * MCBFF-38: Update hardcoded effective location during checkin
    
    * MCBFF-38: fix search query
    
    * MCBFF-38: removed empty lines, added permissions, fixed duplicate logging
    
    * MCBFF-38: given-when-then comments removed
    
    * MCBFF-38: fix fetching service point by location id
    
    * Update src/main/java/org/folio/circulationbff/service/impl/CheckInServiceImpl.java
    
    Co-authored-by: Roman Barannyk <53909129+roman-barannyk@users.noreply.github.com>
    
    * MCBFF-38: fix build
    
    ---------
    
    Co-authored-by: Roman Barannyk <53909129+roman-barannyk@users.noreply.github.com>
    
    (cherry picked from commit 11aa65ca4cb35b98f75798a448ccd1c296f4af20)

--- src/main/java/org/folio/circulationbff/service/impl/CheckInServiceImpl.java
+++ src/main/java/org/folio/circulationbff/service/impl/CheckInServiceImpl.java
@@ -1,13 +1,19 @@
 package org.folio.circulationbff.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.folio.circulationbff.client.feign.CheckInClient;
 import org.folio.circulationbff.domain.dto.CheckInRequest;
 import org.folio.circulationbff.domain.dto.CheckInResponse;
+import org.folio.circulationbff.domain.dto.SearchItem;
 import org.folio.circulationbff.service.CheckInService;
+import org.folio.circulationbff.service.InventoryService;
+import org.folio.circulationbff.service.SearchService;
+import org.folio.circulationbff.service.UserTenantsService;
+import org.folio.spring.service.SystemUserScopedExecutionService;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +21,71 @@ import lombok.extern.log4j.Log4j2;
 public class CheckInServiceImpl implements CheckInService {
 
   private final CheckInClient checkInClient;
+  private final SearchService searchService;
+  private final UserTenantsService userTenantsService;
+  private final InventoryService inventoryService;
+  private final SystemUserScopedExecutionService executionService;
 
   @Override
   public CheckInResponse checkIn(CheckInRequest request) {
     log.info("checkIn: checking in item with barcode {} on service point {}",
-      request::getItemBarcode, request::getServicePointId);
-    return checkInClient.checkIn(request);
+      request.getItemBarcode(), request.getServicePointId());
+    var response = checkInClient.checkIn(request);
+    var item = response.getItem();
+    var servicePointName = getEffectiveLocationServicePoint(item.getId());
+    if (servicePointName != null) {
+      var slipContextItem = response.getStaffSlipContext().getItem();
+      slipContextItem.setEffectiveLocationPrimaryServicePointName(servicePointName);
+      slipContextItem.toServicePoint(servicePointName);
+    }
+    return response;
+  }
+
+  private String getEffectiveLocationServicePoint(String itemId) {
+    log.info("getEffectiveLocationServicePoint: itemId {}", itemId);
+    var instance = searchService.findInstanceByItemId(itemId);
+    if (instance == null) {
+      log.warn("getEffectiveLocationServicePoint: instance not found");
+      return null;
+    }
+    var tenantId = instance.getItems()
+      .stream()
+      .filter(item -> item.getId().equals(itemId))
+      .findFirst()
+      .map(SearchItem::getTenantId)
+      .orElse(null);
+
+    if (Objects.equals(tenantId, userTenantsService.getCurrentTenant())) {
+      log.info("getEffectiveLocationServicePoint: same tenant case {}", tenantId);
+      return fetchServicePointName(itemId);
+    } else {
+      log.info("getEffectiveLocationServicePoint: cross tenant case {}", tenantId);
+      return executionService.executeSystemUserScoped(tenantId, () -> fetchServicePointName(itemId));
+    }
+  }
+
+  private String fetchServicePointName(String itemId) {
+    var item = inventoryService.fetchItem(itemId);
+    if (item == null) {
+      log.warn("fetchServicePointName:: item not found, itemId: {}", itemId);
+      return null;
+    }
+    var location = inventoryService.fetchLocation(item.getEffectiveLocationId());
+    if (location == null) {
+      log.warn("fetchServicePointName:: location not found, locationId: {}",
+        item.getEffectiveLocationId());
+      return null;
+    }
+    var servicePoint = inventoryService.fetchServicePoint(location.getPrimaryServicePoint().toString());
+    if (servicePoint == null) {
+      log.warn("fetchServicePointName:: servicePoint not found, servicePointId: {}",
+        location.getPrimaryServicePoint());
+      return null;
+    }
+    String servicePointName = servicePoint.getName();
+    log.info("fetchServicePointName:: result: {}", servicePointName);
+
+    return servicePointName;
   }
 
 }
---
+++ src/main/java/org/folio/circulationbff/service/impl/InventoryServiceImpl.java
@@ -0,0 +1,39 @@
+package org.folio.circulationbff.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.folio.circulationbff.client.feign.ItemStorageClient;
+import org.folio.circulationbff.client.feign.LocationClient;
+import org.folio.circulationbff.client.feign.ServicePointClient;
+import org.folio.circulationbff.domain.dto.Item;
+import org.folio.circulationbff.domain.dto.Location;
+import org.folio.circulationbff.domain.dto.ServicePoint;
+import org.folio.circulationbff.service.InventoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class InventoryServiceImpl implements InventoryService {
+  private final ItemStorageClient itemClient;
+  private final LocationClient locationClient;
+  private final ServicePointClient servicePointClient;
+
+  @Override
+  public Item fetchItem(String id) {
+    log.info("fetchItem:: fetching item {}", id);
+    return itemClient.findItem(id);
+  }
+
+  @Override
+  public Location fetchLocation(String id) {
+    log.info("fetchLocation:: fetching location {}", id);
+    return locationClient.findLocation(id);
+  }
+
+  @Override
+  public ServicePoint fetchServicePoint(String id) {
+    log.info("fetchServicePoint:: fetching service point {}", id);
+    return servicePointClient.findServicePoint(id);
+  }
+}
