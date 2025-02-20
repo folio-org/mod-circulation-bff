3d81fd958c78824e21f8a4ca55b38e8194c1531a
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2025-02-20 15:02:13 +0100

    MCBFF-46 Revert redundant changes

--- src/main/java/org/folio/circulationbff/service/SettingsService.java
+++ src/main/java/org/folio/circulationbff/service/SettingsService.java
@@ -3,5 +3,4 @@ package org.folio.circulationbff.service;
 public interface SettingsService {
   boolean isEcsTlrFeatureEnabled();
   boolean isEcsTlrFeatureEnabled(String tenantId);
-  boolean isEcsTlrFeatureEnabled(boolean isCentralTenant);
 }
