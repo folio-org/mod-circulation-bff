43882e4b218f2b0f831452728573acc033956046
Roman Barannyk <53909129+roman-barannyk@users.noreply.github.com>
2025-07-29 17:00:25 +0300

    MCBFF-115 Implement claim-item-returned API (#100)
    * MCBFF-114 Implement claim-item-returned API
    
    * MCBFF-114 update test
    
    * MCBFF-115 fix formatting
    
    * MCBFF-115 revert redundant changes
    
    * MCBFF-115 incorporating review comments
    
    * MCBFF-115 incorporating review comments
    
    * MCBFF-115 refactoring
    
    * MCBFF-115 refactoring
    
    * MCBFF-115 update logging
    
    * MCBFF-115 refactoring, fix code smell
    
    * MCBFF-115 fix typo, update dependency
    
    * MCBFF-115 add mapper
    
    * MCBFF-115 add tests
    
    * MCBFF-115 use import for UUID

--- src/test/java/org/folio/circulationbff/api/CirculationBffDeclareItemLostApiTest.java
+++ src/test/java/org/folio/circulationbff/api/CirculationBffDeclareItemLostApiTest.java
@@ -1,14 +1,13 @@
 package org.folio.circulationbff.api;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.jsonResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.noContent;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static org.mockito.Mockito.when;
+import static java.lang.String.format;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static wiremock.org.apache.hc.core5.http.HttpStatus.SC_NO_CONTENT;
 
 import java.util.Date;
 import java.util.UUID;
@@ -35,13 +34,13 @@ class CirculationBffDeclareItemLostApiTest extends BaseIT {
   @Test
   @SneakyThrows
   void callsCirculationWhenEcsTlrDisabled() {
-    var loanId = UUID.randomUUID().toString();
-    var userTenant = new UserTenant(UUID.randomUUID().toString(), TENANT_ID_COLLEGE);
+    var loanId = randomId();
+    var userTenant = new UserTenant(randomId(), TENANT_ID_COLLEGE);
     userTenant.setCentralTenantId(TENANT_ID_CONSORTIUM);
     mockHelper.mockUserTenants(userTenant, TENANT_ID_COLLEGE);
     mockHelper.mockEcsTlrCirculationSettings(false, TENANT_ID_COLLEGE);
 
-    wireMockServer.stubFor(WireMock.post(urlPathEqualTo(String.format(CIRCULATION_DECLARE_ITEM_LOST_URL, loanId)))
+    wireMockServer.stubFor(WireMock.post(urlPathEqualTo(format(CIRCULATION_DECLARE_ITEM_LOST_URL, loanId)))
       .withHeader(HEADER_TENANT, equalTo(TENANT_ID_COLLEGE))
       .willReturn(WireMock.noContent()));
 
@@ -49,77 +48,75 @@ class CirculationBffDeclareItemLostApiTest extends BaseIT {
       .declaredLostDateTime(new Date())
       .servicePointId(UUID.randomUUID()));
 
-    wireMockServer.verify(postRequestedFor(urlPathEqualTo(String.format(CIRCULATION_DECLARE_ITEM_LOST_URL, loanId)))
+    wireMockServer.verify(postRequestedFor(urlPathEqualTo(format(CIRCULATION_DECLARE_ITEM_LOST_URL, loanId)))
       .withHeader(HEADER_TENANT, equalTo(TENANT_ID_COLLEGE)));
   }
 
   @Test
   @SneakyThrows
   void callsTlrWhenEcsTlrEnabledInCentralTenant() {
-    var loanId = UUID.randomUUID().toString();
-    var userTenant = new UserTenant(UUID.randomUUID().toString(), TENANT_ID_CONSORTIUM);
+    var loanId = randomId();
+    var userTenant = new UserTenant(randomId(), TENANT_ID_CONSORTIUM);
     userTenant.setCentralTenantId(TENANT_ID_CONSORTIUM);
     mockHelper.mockUserTenants(userTenant, TENANT_ID_CONSORTIUM);
     mockHelper.mockEcsTlrSettings(true);
 
-    wireMockServer.stubFor(WireMock.post(urlPathEqualTo(String.format(TLR_DECLARE_ITEM_LOST_URL, loanId)))
+    wireMockServer.stubFor(WireMock.post(urlPathEqualTo(format(TLR_DECLARE_ITEM_LOST_URL, loanId)))
       .withHeader(HEADER_TENANT, equalTo(TENANT_ID_CONSORTIUM))
-      .willReturn(jsonResponse("", SC_NO_CONTENT)));
+      .willReturn(noContent()));
 
     performDeclareItemLost(TENANT_ID_CONSORTIUM, loanId, new DeclareItemLostRequest()
       .declaredLostDateTime(new Date())
       .servicePointId(UUID.randomUUID()));
 
-    wireMockServer.verify(postRequestedFor(urlPathEqualTo(String.format(TLR_DECLARE_ITEM_LOST_URL, loanId)))
+    wireMockServer.verify(postRequestedFor(urlPathEqualTo(format(TLR_DECLARE_ITEM_LOST_URL, loanId)))
       .withHeader(HEADER_TENANT, equalTo(TENANT_ID_CONSORTIUM)));
   }
 
   @Test
   @SneakyThrows
   void callsRequestMediatedWhenEcsTlrEnabledAndCurrentTenantSecure() {
-    var loanId = UUID.randomUUID().toString();
-    var userTenant = new UserTenant(UUID.randomUUID().toString(), TENANT_ID_SECURE);
+    var loanId = randomId();
+    var userTenant = new UserTenant(randomId(), TENANT_ID_SECURE);
     userTenant.setCentralTenantId(TENANT_ID_CONSORTIUM);
     mockHelper.mockUserTenants(userTenant, TENANT_ID_SECURE);
     mockHelper.mockEcsTlrCirculationSettings(true, TENANT_ID_SECURE);
-    when(tenantService.isCurrentTenantSecure()).thenReturn(true);
 
-    wireMockServer.stubFor(WireMock.post(urlPathEqualTo(String.format(REQUESTS_MEDIATED_DECLARE_ITEM_LOST_URL, loanId)))
+    wireMockServer.stubFor(WireMock.post(urlPathEqualTo(format(REQUESTS_MEDIATED_DECLARE_ITEM_LOST_URL, loanId)))
       .withHeader(HEADER_TENANT, equalTo(TENANT_ID_SECURE))
-      .willReturn(jsonResponse("", SC_NO_CONTENT)));
+      .willReturn(noContent()));
 
     performDeclareItemLost(TENANT_ID_SECURE, loanId, new DeclareItemLostRequest()
       .declaredLostDateTime(new Date())
       .servicePointId(UUID.randomUUID()));
 
-    wireMockServer.verify(postRequestedFor(urlPathEqualTo(String.format(REQUESTS_MEDIATED_DECLARE_ITEM_LOST_URL, loanId)))
+    wireMockServer.verify(postRequestedFor(urlPathEqualTo(format(REQUESTS_MEDIATED_DECLARE_ITEM_LOST_URL, loanId)))
       .withHeader(HEADER_TENANT, equalTo(TENANT_ID_SECURE)));
   }
 
   @Test
   @SneakyThrows
   void fallbackToCirculationWhenEcsTlrEnabledButNotCentralOrSecure() {
-    var loanId = UUID.randomUUID().toString();
-    var userTenant = new UserTenant(UUID.randomUUID().toString(), TENANT_ID_COLLEGE);
+    var loanId = randomId();
+    var userTenant = new UserTenant(randomId(), TENANT_ID_COLLEGE);
     userTenant.setCentralTenantId(TENANT_ID_CONSORTIUM);
     mockHelper.mockUserTenants(userTenant, TENANT_ID_COLLEGE);
     mockHelper.mockEcsTlrCirculationSettings(true, TENANT_ID_COLLEGE);
-    when(tenantService.isCurrentTenantSecure()).thenReturn(false);
 
-    wireMockServer.stubFor(WireMock.post(urlPathEqualTo(String.format(CIRCULATION_DECLARE_ITEM_LOST_URL, loanId)))
+    wireMockServer.stubFor(WireMock.post(urlPathEqualTo(format(CIRCULATION_DECLARE_ITEM_LOST_URL, loanId)))
       .withHeader(HEADER_TENANT, equalTo(TENANT_ID_COLLEGE))
-      .willReturn(jsonResponse("", SC_NO_CONTENT)));
+      .willReturn(noContent()));
 
     performDeclareItemLost(TENANT_ID_COLLEGE, loanId, new DeclareItemLostRequest()
       .declaredLostDateTime(new Date())
       .servicePointId(UUID.randomUUID()));
 
-    wireMockServer.verify(postRequestedFor(urlPathEqualTo(String.format(CIRCULATION_DECLARE_ITEM_LOST_URL, loanId)))
+    wireMockServer.verify(postRequestedFor(urlPathEqualTo(format(CIRCULATION_DECLARE_ITEM_LOST_URL, loanId)))
       .withHeader(HEADER_TENANT, equalTo(TENANT_ID_COLLEGE)));
   }
 
   private void performDeclareItemLost(String tenantId, String loanId, DeclareItemLostRequest request) throws Exception {
-    mockMvc.perform(post(String.format(DECLARE_ITEM_LOST_URL, loanId))
+    mockMvc.perform(post(format(DECLARE_ITEM_LOST_URL, loanId))
         .headers(buildHeaders(tenantId))
         .contentType(APPLICATION_JSON)
         .content(asJsonString(request)))
