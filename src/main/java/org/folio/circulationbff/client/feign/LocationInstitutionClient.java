bddc93d59dd1f9303424be3032cc4e6157cbb9bd
OleksandrVidinieiev <56632770+OleksandrVidinieiev@users.noreply.github.com>
2025-08-01 23:56:38 +0200

    MCBFF-134: Search instances with `expandAll=true` (#106)
    * MCBFF-134 Add support for additional query params to GetByQueryClient
    
    * MCBFF-134 Remove unused argument captor
    
    * MCBFF-134 Refactor tests
    
    * MCBFF-134 Mock search with `expandAll=true`
    
    * MCBFF-134 Test for query and limit override
    
    * MCBFF-134 Remove unused import

--- src/main/java/org/folio/circulationbff/client/feign/LocationInstitutionClient.java
+++ src/main/java/org/folio/circulationbff/client/feign/LocationInstitutionClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "institutions", url = "location-units/institutions", configuration = FeignClientConfiguration.class)
-public interface LocationInstitutionClient extends GetByQueryClient<Institutions> {
+public interface LocationInstitutionClient extends GetByQueryParamsClient<Institutions> {
 
   @GetMapping("/{id}")
   Institution findInstitution(@PathVariable String id);
