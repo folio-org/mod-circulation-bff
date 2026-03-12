7d46773fb114a21a217fd4f18981ceec44ac13c8
alexanderkurash <alexander.kurash@gmail.com>
2026-03-12 18:51:18 +0200

    MCBFF-157 Remove unneeded annotations

--- src/main/java/org/folio/circulationbff/client/GetByQueryParamsClient.java
+++ src/main/java/org/folio/circulationbff/client/GetByQueryParamsClient.java
@@ -4,13 +4,8 @@ import java.util.Map;
 
 import org.folio.circulationbff.support.CqlQuery;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.service.annotation.GetExchange;
 
 public interface GetByQueryParamsClient<T> {
-
-  @GetExchange
   T getByQuery(@RequestParam CqlQuery query, @RequestParam int limit);
-
-  @GetExchange
   T getByQueryParams(@RequestParam Map<String, String> queryParams);
 }
